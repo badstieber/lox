dee5b78600bbbd73b3bd52d48674c0b5cf29024b
badstieber <pascal.badstieber@gmx.de>
2022-12-15 16:37:46 +0100

    add support for identifiers and keywords

--- jlox/src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -13,6 +13,26 @@ class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+    private static final Map<String, TokenType> keywords = new HashMap<>();
+
+    static {
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("fun", FUN);
+        keywords.put("for", FOR);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
 
     Scanner(String source) {
         this.source = source;
@@ -48,20 +68,49 @@ class Scanner {
             case '/' -> {
                 if (match('/')) {
                     // A comment goes until the end of the line
-                    while (peek() != '\n' && !isAtEnd()) {
-                        advance();
-                    }
+                    while (peek() != '\n' && !isAtEnd()) advance();
                 } else {
                     addToken(SLASH);
                 }
             }
-            case ' ', '\t', '\r' -> {}  // ignore newlines and whitespace:
+            case ' ', '\t', '\r' -> {
+            }  // ignore newlines and whitespace:
             case '\n' -> line++;
             case '"' -> string();
 
-
-            default -> Lox.error(line, "Unexpected character.");
+            default -> {
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
+            }
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+
+        addToken(type);
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part:
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance();
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
     }
 
     private void string() {
@@ -77,7 +126,7 @@ class Scanner {
             return;
         }
 
-        // Here we are at the closing '"':
+        // Here we are at the closing quote symbol:
         advance();
 
         // Finally trim the surrounding quotes:
@@ -100,6 +149,25 @@ class Scanner {
 
     private char peek() {
         return isAtEnd() ? '\0' : source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c) {
+        return ('a' <= c && c <= 'z') ||
+                ('A' <= c && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isDigit(char c) {
+        return '0' <= c && c <= '9';
     }
 
     private boolean isAtEnd() {
